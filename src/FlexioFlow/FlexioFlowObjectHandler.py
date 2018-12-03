28da6d1f773fce6c6849cabeb0bc8fa15d63bb5c
thomas <thomas@flexio.fr>
2018-12-03 22:31:34 +0100

    parse version

--- src/FlexioFlow/FlexioFlowObjectHandler.py
+++ src/FlexioFlow/FlexioFlowObjectHandler.py
@@ -4,12 +4,17 @@ from FlexioFlow.FlexioFlowValueObject import FlexioFlowValueObject
 from FlexioFlow.Level import Level
 from FlexioFlow.Scheme import Scheme
 from utils.EnumUtils import EnumUtils
+from typing import Tuple, NewType
+import re
+
+version_type = NewType('version', Tuple[int, int, int])
 
 
 class FlexioFlowObjectHandler:
     __state: FlexioFlowValueObject
     file_path: str
     FILE_NAME = 'flexio-flow.yml'
+    __version: version_type
 
     def __init__(self, file_path: str):
         if not os.path.exists(file_path):
@@ -24,6 +29,22 @@ class FlexioFlowObjectHandler:
     def state(self, v: FlexioFlowValueObject):
         self.__state = v
 
+    @property
+    def version(self) -> version_type:
+        return self.__version
+
+    @version.setter
+    def version(self, v: version_type):
+        if not isinstance(v, version_type):
+            raise TypeError('version should be an instance of version_type')
+        self.__version = v
+
+    def versionFromStr(self, v: str) -> version_type:
+        matches = re.match('^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)$', v)
+        return (int(matches.groupdict().get('major')),
+                int(matches.groupdict().get('minor')),
+                int(matches.groupdict().get('patch')))
+
     def loadFileConfig(self):
         data = yaml.load(open(self.filePath(), 'r'))
 
@@ -32,6 +53,8 @@ class FlexioFlowObjectHandler:
             scheme=EnumUtils(Scheme).list_from_value(data['scheme']),
             level=Level(data['level']))
         print(repr(self.__state.to_dict()))
+        print(self.versionFromStr(data['version']))
+        self.version = self.versionFromStr(data['version'])
 
     def filePath(self) -> str:
         return self.file_path + '/' + self.FILE_NAME
