c8e1a79d826ab8827e2614325a8815839885f055
thomas <thomas@flexio.fr>
2018-12-04 17:55:05 +0100

    add version schema

--- src/FlexioFlow/FlexioFlowObjectHandler.py
+++ src/FlexioFlow/FlexioFlowObjectHandler.py
@@ -3,18 +3,17 @@ import os
 from FlexioFlow.FlexioFlowValueObject import FlexioFlowValueObject
 from FlexioFlow.Level import Level
 from FlexioFlow.Scheme import Scheme
+from FlexioFlow.Version import Version
 from utils.EnumUtils import EnumUtils
 from typing import Tuple, NewType
 import re
-
-version_type = NewType('version', Tuple[int, int, int])
 
 
 class FlexioFlowObjectHandler:
     __state: FlexioFlowValueObject
     file_path: str
     FILE_NAME = 'flexio-flow.yml'
-    __version: version_type
+    __version: Version
 
     def __init__(self, file_path: str):
         if not os.path.exists(file_path):
@@ -30,20 +29,14 @@ class FlexioFlowObjectHandler:
         self.__state = v
 
     @property
-    def version(self) -> version_type:
+    def version(self) -> Version:
         return self.__version
 
     @version.setter
-    def version(self, v: version_type):
-        if not isinstance(v, version_type):
-            raise TypeError('version should be an instance of version_type')
+    def version(self, v: Version):
+        if not isinstance(v, Version):
+            raise TypeError('version should be an instance of FlexioFlow.Version')
         self.__version = v
-
-    def versionFromStr(self, v: str) -> version_type:
-        matches = re.match('^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)$', v)
-        return (int(matches.groupdict().get('major')),
-                int(matches.groupdict().get('minor')),
-                int(matches.groupdict().get('patch')))
 
     def loadFileConfig(self):
         data = yaml.load(open(self.filePath(), 'r'))
@@ -53,8 +46,8 @@ class FlexioFlowObjectHandler:
             scheme=EnumUtils(Scheme).list_from_value(data['scheme']),
             level=Level(data['level']))
         print(repr(self.__state.to_dict()))
-        print(self.versionFromStr(data['version']))
-        self.version = self.versionFromStr(data['version'])
+        self.version = Version.fromStr(data['version'])
+        print(self.version)
 
     def filePath(self) -> str:
         return self.file_path + '/' + self.FILE_NAME
---
+++ src/FlexioFlow/Version.py
@@ -0,0 +1,57 @@
+import re
+from typing import Dict
+from __future__ import annotations
+
+
+class Version:
+    SEP: str = '.'
+
+    major: int = 0
+    minor: int = 0
+    patch: int = 0
+
+    def __init__(self, major: int, minor: int, patch: int) -> None:
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+
+    @staticmethod
+    def fromStr(v: str) -> 'Version':
+        matches = re.match('^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)$', v)
+        return Version(
+            major=int(matches.groupdict().get('major')),
+            minor=int(matches.groupdict().get('minor')),
+            patch=int(matches.groupdict().get('patch'))
+        )
+
+    def major(self) -> 'Version':
+        self.major += 1
+        return self
+
+    def minor(self) -> 'Version':
+        self.minor += 1
+        return self
+
+    def patch(self) -> 'Version':
+        self.patch += 1
+        return self
+
+    def reset_patch(self) -> 'Version':
+        self.patch = 0
+        return self
+
+    def to_dict(self) -> Dict[str, int]:
+        return {
+            'major': self.major,
+            'minor': self.minor,
+            'patch': self.patch
+        }
+
+    def __str__(self):
+        return str(
+            self.SEP.join(
+                list(
+                    map(lambda x: str(x), self.to_dict().values())
+                )
+            )
+        )
