cc4f96595437722ce9cc42e91387d7eca90a7b15
thomas <thomas@flexio.fr>
2018-12-10 18:37:14 +0100

    begin cmd

--- src/VersionControl/GitFlow/Branches/Hotfix/Hotfix.py
+++ src/VersionControl/GitFlow/Branches/Hotfix/Hotfix.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 from VersionControl.Branch import Branch
 from FlexioFlow.Actions.Actions import Actions
+from VersionControl.GitFlow.Branches.Hotfix.Start import Start
 
 class Hotfix(Branch):
 
     def process(self):
         if self.action is Actions.START:
             print('Hotfix start')
+            Start.process(self.state_handler.dir_path)
         if self.action is Actions.FINISH:
             print('Hotfix finish')
         print(self.__dict__)
--- src/VersionControl/GitFlow/Branches/Hotfix/Start.py
+++ src/VersionControl/GitFlow/Branches/Hotfix/Start.py
@@ -1,3 +1,24 @@
+from pathlib import Path
+import shlex, subprocess
+import os
+import shutil
+
+
+
 class Start:
     def __init__(self):
         pass
+
+    @staticmethod
+    def process(dir_path: Path):
+        workdir: Path = Path('/tmp/flexio_flow_tests')
+        shutil.rmtree(workdir)
+        workdir.mkdir()
+
+        print(workdir.as_posix())
+
+        subprocess.Popen(["pwd"])
+        os.chdir(workdir)
+        # subprocess.Popen(["cd", workdir.as_posix()])
+        subprocess.Popen(["pwd"])
+        subprocess.Popen(["git", "flow", "init"])
