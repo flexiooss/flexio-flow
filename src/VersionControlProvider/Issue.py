92f2a3b9ec4eccab1934c17c729c1be15e208722
Thomas-C <thomas@flexio.fr>
2018-12-19 20:17:10 +0100

    update issue schema

--- src/VersionControlProvider/Issue.py
+++ src/VersionControlProvider/Issue.py
@@ -1,23 +1,36 @@
 from __future__ import annotations
 
-from typing import List, Dict
+from typing import List, Dict, Optional
+
+from VersionControlProvider.IssueState import IssueState
 
 
 class Issue:
     PREFIX: str = '#'
-    number: int
-    title: str
-    body: str
-    milestone: int
-    state
+    number: Optional[int]
+    title: Optional[str]
+    body: Optional[str]
+    milestone: Optional[int]
+    state: Optional[IssueState]
     labels: List[str]
     assignees: List[str]
 
     def __init__(self):
-        self.assignees= []
-        self.labels= []
+        self.number = None
+        self.title = None
+        self.body = None
+        self.milestone = None
+        self.state = None
+        self.assignees = []
+        self.labels = []
+
+    def with_number(self, number: int) -> Issue:
+        self.number = number
+        return self
 
     def get_ref(self) -> str:
+        if self.number is None:
+            raise ValueError('Issue should have a number')
         return '{prefix!s}{number!s}'.format(prefix=self.PREFIX, number=self.number)
 
     def assign(self, user: str) -> Issue:
@@ -34,8 +47,12 @@ class Issue:
         issue: dict = {
             'title': self.title
         }
-        if self.body:
+        if self.body is not None:
             issue['body'] = self.body
+        if self.milestone is not None:
+            issue['milestone'] = self.milestone
+        if self.state is not None:
+            issue['state'] = self.state.value
         if len(self.labels):
             issue['labels'] = self.labels
         if len(self.assignees):
