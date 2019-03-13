874303ab841c8aa4f4042e16310babade63addee
tomacht <thomas@flexio.fr>
2019-03-13 12:56:27 +0100

    add name prop for Branch

--- src/VersionControl/Branch.py
+++ src/VersionControl/Branch.py
@@ -9,13 +9,20 @@ from VersionControlProvider.Issue import Issue
 
 class Branch(abc.ABC):
     action: Actions
+    issue: Optional[Issue]
+    name: Optional[str]
 
     def __init__(self, state_handler: StateHandler) -> None:
         self.state_handler: StateHandler = state_handler
         self.issue: Optional[Issue] = None
+        self.name: Optional[str] = None
 
     def with_issue(self, issue: Issue) -> Branch:
         self.issue = issue
+        return self
+
+    def with_name(self, name: str) -> Branch:
+        self.name = name
         return self
 
     @abc.abstractmethod
