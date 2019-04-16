608a62f97f31abc21ea8db83a7f9f115be315e6a
tomacht <thomas@flexio.fr>
2019-04-16 08:08:24 +0200

    add list dependencies

--- src/Branches/Actions/PreCheck.py
+++ src/Branches/Actions/PreCheck.py
@@ -20,4 +20,4 @@ class PreCheck(Action):
             dev_dependencies: Dependencies = sc.release_precheck()
             if len(dev_dependencies):
                 raise HaveDevDependencyException(dev_dependencies)
-            # print(dev_dependencies.__dict__())
+            print(dev_dependencies.__dict__())
