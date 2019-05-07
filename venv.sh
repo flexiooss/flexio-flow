331df886712b9ce798bf0cf4cdf490685d7fb1bf
tomacht <thomas@flexio.fr>
2019-05-07 14:55:59 +0200

    fix venv python version

--- venv.sh
+++ venv.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -e
-if [ ! -f /usr/bin/python3.7 ]; then
-    echo "Python 3.7 is required"
-    exit 1
-fi
 python3.7 -m venv $PWD/venv
 source $PWD/venv/bin/activate
 python3.7 -m pip install --upgrade pip
