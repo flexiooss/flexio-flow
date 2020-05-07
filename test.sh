b5da2c45a82b8197ff6c85c8dd8b88de1fd60abe
tomacht <thomas@flexio.fr>
2020-05-07 13:54:15 +0200

    rm strict python version

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@ ROOT_PWD=$PWD
 cd $PWD'/src'
 if [ $# -ne 0 ]
 then
-  python3.7 -m unittest "$@"
+  python3 -m unittest "$@"
 else
-  python3.7 -m unittest tests.tests
+  python3 -m unittest tests.tests
 fi
 
 cd $ROOT_PWD
